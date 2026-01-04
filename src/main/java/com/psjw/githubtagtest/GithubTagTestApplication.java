823c3bca235b3c9b2c9940be590e14bb48b2f1f4
psjw <dlrudtn108@gmail.com>
2026-01-04 09:35:04 +0900

    first commit

---
+++ src/main/java/com/psjw/githubtagtest/GithubTagTestApplication.java
@@ -0,0 +1,13 @@
+package com.psjw.githubtagtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubTagTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubTagTestApplication.class, args);
+    }
+
+}
